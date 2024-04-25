26273ffa20490b4fa35e4dfa945cd6604295c83d
John Swaynos <jpswaynos@gmail.com>
2024-04-24 20:20:04 -0700

    Forgot to join the threads and using the logging library instead of printing to the console.

--- src/start.py
+++ src/start.py
@@ -1,5 +1,6 @@
 from AppKit import NSApplicationActivateAllWindows, NSApplicationActivateIgnoringOtherApps
 import json
+import logging
 import requests
 import threading
 import time
@@ -17,12 +18,10 @@ app = find_app(config.APP_NAME)
 pid = app.processIdentifier()
 controller = PlaystationController()
 
-# TODO: Better logging
-
 # TODO: Remove or place in another file
 def compare_image_to_screenshot(image, screenshot_name):
     ssimResult = image.compare_ssim(screenshot_name)
-    print("SSIM Result for screeshot is {} for screenshot {}".format(ssimResult, screenshot_name))
+    logging.info("SSIM Result for screeshot is {} for screenshot {}".format(ssimResult, screenshot_name))
 
 # Begin Threading
 # Create a mutex object for the most recent screenshot
@@ -49,7 +48,7 @@ def activate_app(thread_name=""):
         activationResult = app.activateWithOptions_(NSApplicationActivateAllWindows | NSApplicationActivateIgnoringOtherApps)
 
         if not (activationResult):
-            print(f"{thread_name}: app activation failed")
+            logging.error(f"{thread_name}: app activation failed")
 
 
 def capture_image_thread():
@@ -59,18 +58,21 @@ def capture_image_thread():
     The image is then locked with the `latest_screenshot_mutex` lock, ensuring that only one thread can access it at a time.
     """
     while(app):
-        print(f"capture_image_thread: Has looped {capture_image_thread_statistics.count} times. Elapsed time is {capture_image_thread_statistics.get_time()}")
-        activate_app("capture_image_thread")
-        print("capture_image_thread: Grabbing a screenshot")
-        window = get_window(pid)
-        image = ImageWrapper(get_image_from_window(window))
-        # Lock the image so that if another thread is trying to read, we aren't overwritting it
-        with latest_screenshots_mutex:
-            # clear the stack if it gets too big
-            if (len(latest_screenshots_stack) > 10):
-                latest_screenshots_stack.clear()
-            latest_screenshots_stack.append(image)
-        capture_image_thread_statistics.count += 1
+        try:
+            logging.debug(f"capture_image_thread: Has looped {capture_image_thread_statistics.count} times. Elapsed time is {capture_image_thread_statistics.get_time()}")
+            activate_app("capture_image_thread")
+            logging.info("capture_image_thread: Grabbing a screenshot")
+            window = get_window(pid)
+            image = ImageWrapper(get_image_from_window(window))
+            # Lock the image so that if another thread is trying to read, we aren't overwritting it
+            with latest_screenshots_mutex:
+                # clear the stack if it gets too big
+                if (len(latest_screenshots_stack) > 10):
+                    latest_screenshots_stack.clear()
+                latest_screenshots_stack.append(image)
+            capture_image_thread_statistics.count += 1
+        except Exception as argument:
+            logging.error(argument)
 
 def infer_image_thread():
     """
@@ -79,26 +81,29 @@ def infer_image_thread():
     The inferred image is then stored in a global variable for later use, ensuring that only one thread can access it at a time.
     """
     while(True):
-        image = None
-        with latest_screenshots_mutex:
-            if (len(latest_screenshots_stack) > 0):
-                image = latest_screenshots_stack.pop()
-        if(image != None):
-            print("infer_image_thread: Has looped {} times. Elapsed time is {}".format(infer_image_thread_statistics.count, infer_image_thread_statistics.get_time()))
-            print("infer_image_thread: inferring image from latest screenshot using ollama")
-            payload = {
-                "model": "llava",
-                "prompt": get_prompt("screenshot_prompt.txt"),
-                "stream": False,
-                "images": [f"{image.scaled_as_base64()}"]
-            }
-            response = requests.post(config.OLLAMA_URL, json=payload)
-            responseObj = json.loads(response.text)
-            print(responseObj['response'])
-        else:
-            print("infer_image_thread: There is not a latest screenshot to infer from")
-            time.sleep(1)
-        infer_image_thread_statistics.count += 1
+        try:
+            image = None
+            with latest_screenshots_mutex:
+                if (len(latest_screenshots_stack) > 0):
+                    image = latest_screenshots_stack.pop()
+            if(image != None):
+                logging.debug("infer_image_thread: Has looped {} times. Elapsed time is {}".format(infer_image_thread_statistics.count, infer_image_thread_statistics.get_time()))
+                logging.info("infer_image_thread: inferring image from latest screenshot using ollama")
+                payload = {
+                    "model": "llava",
+                    "prompt": get_prompt("screenshot_prompt.txt"),
+                    "stream": False,
+                    "images": [f"{image.scaled_as_base64()}"]
+                }
+                response = requests.post(config.OLLAMA_URL, json=payload)
+                responseObj = json.loads(response.text)
+                logging.info(responseObj['response']) # TODO: do something
+            else:
+                logging.debug("infer_image_thread: There is not a latest screenshot to infer from")
+                time.sleep(1)
+            infer_image_thread_statistics.count += 1
+        except Exception as argument:
+            logging.error(argument)
     
 def controller_input_thread():
     """
@@ -106,16 +111,18 @@ def controller_input_thread():
     It uses the `controller` module to grab the latest input data for each button on the controller and performs actions based on those inputs.
     """
     while(controller):
-        # TODO: uncomment logs (they're noisy).
-        # TODO: Only enter if the right window is active. (Annoying that keystrokes are entered while debugging)
-        # print(f"controller_input_thread: Has looped {controller_input_thread_statistics.count} times. Elapsed time is {controller_input_thread_statistics.get_time()}")
+        try:
+            # TODO: Only enter if the right window is active. (Annoying that keystrokes are entered while debugging)
+            logging.debug(f"controller_input_thread: Has looped {controller_input_thread_statistics.count} times. Elapsed time is {controller_input_thread_statistics.get_time()}")
 
-        # press, release, tap to send input to the controller. Joystick movement is special.
-        # print("controller_input_thread: tapping Cross button")
-        controller.tap(PlaystationController.Cross)
+            # press, release, tap to send input to the controller. Joystick movement is special.
+            logging.info("controller_input_thread: tapping Cross button")
+            controller.tap(PlaystationController.Cross)
 
-        time.sleep(.1)
-        controller_input_thread_statistics.count += 1
+            time.sleep(.1)
+            controller_input_thread_statistics.count += 1
+        except Exception as argument:
+            logging.error(argument)
 
 # Create a new Thread object for each function
 capture_image_thread_instance = threading.Thread(target=capture_image_thread)
@@ -128,3 +135,7 @@ activate_app(app)
 capture_image_thread_instance.start()
 infer_image_thread_instance.start()
 controller_input_thread_instance.start()
+
+capture_image_thread_instance.join()
+infer_image_thread_instance.join()
+controller_input_thread_instance.join()
