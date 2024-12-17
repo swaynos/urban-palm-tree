7c474a727249105ccee58328627b42c1c2d887ac
John Swaynos <jpswaynos@gmail.com>
2024-12-17 14:24:09 -0800

    Adding latest changes to run inference from the notebook.

--- src/yolo_image_classification_inference.py
+++ src/yolo_image_classification_inference.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 import numpy as np
 import tensorflow as tf
+from ultralytics import YOLO
 from huggingface_hub import hf_hub_download
 
 import config
@@ -10,19 +11,18 @@ logger = logging.getLogger(__name__)
 
 class YoloImageClassifier:
 
-    def __init__(self, modelpath, filename, class_labels, target_resolution=(640, 640)):
+    def __init__(self, modelpath, filename, target_resolution=(640, 640)):
         self.modelpath = hf_hub_download(modelpath, filename)
-        self.model = tf.keras.models.load_model(self.modelpath)
-        # Compile the loaded model specifying metrics
-        self.model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        self.model = YOLO(self.modelpath)
 
-        self.class_labels = class_labels
         self.target_resolution = target_resolution
 
     async def classify_image(self, image_wrapper):
         logger.debug(f"Classifying image from latest screenshot using {self.modelpath}")
         img = await self.load_and_preprocess_image(image_wrapper)
-        predictions = await asyncio.to_thread(self.model.predict, img)
+        
+        results = self.model.predict(source=img, imgsz=640, conf=0.25)
+        #predictions = await asyncio.to_thread(self.model.predict, img)
 
         predicted_classes = [self.class_labels[np.argmax(prediction)] for prediction in predictions]
 
