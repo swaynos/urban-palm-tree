b936dc09c44ee41932478718bdd2ea01b46b44dc
John Swaynos <jpswaynos@gmail.com>
2025-03-08 11:10:29 -0800

    Circular imports with Actions. The architecture needs revision.

--- src/controllers/game_flow_controller.py
+++ src/controllers/game_flow_controller.py
@@ -5,12 +5,12 @@ from pynput import keyboard as kb
 from typing import List
 
 from controllers.game_strategy_controller import GameStrategyController
-from game_action.action import Action
 from utilities.playstation_io import PlaystationIO
 
 class GameFlowController():
     def __init__(self):
         self.io = PlaystationIO()
+        self.actions = []
 
     async def press_button(self, button: kb.KeyCode, duration: float = 0.1):
         try:
@@ -57,17 +57,10 @@ class GameFlowController():
             self.io.release_all_buttons() # Ensure all buttons are released
             pass
 
-    async def build_actions(self, game_strategy: GameStrategyController)-> list[Action]:
+    async def build_actions(self, game_strategy: GameStrategyController):
         """
         Builds a list of actions to be performed based on the current game state and strategy.
         """
-        actions = []
         # TODO: Implement the logic to build actions based on the game state and strategy
-        return actions
-    
-    async def execute_actions(self, actions: list[Action]):
-        """
-        Executes a list of actions.
-        """
-        # TODO: Implement the logic to execute actions
+        # self.actions
         pass
